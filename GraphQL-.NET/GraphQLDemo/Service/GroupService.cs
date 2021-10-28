44ddcb052949a9b3270b3415667f3d7cc3ffe7bf
Vlad Luchian <62203002+vladluc99@users.noreply.github.com>
2021-10-28 13:32:32 +0300

    Add files via upload

---
+++ GraphQL-.NET/GraphQLDemo/Service/GroupService.cs
@@ -0,0 +1,25 @@
+﻿using GraphQLDemo.IService;
+using GraphQLDemo.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphQLDemo.Service
+{
+    public class GroupService : IGroupService
+    {
+        private IList<Group> _groups;
+        public GroupService()
+        {
+            _groups = new List<Group>()
+            {
+                new Group(){GroupId = 1, Name = "Science", ShortName = "SC"},
+                new Group(){GroupId = 2, Name = "Commerce", ShortName = "COM"},
+                new Group(){GroupId = 3, Name = "Arts", ShortName = "AR"}
+            };
+        }
+        public IQueryable<Group> GetAll()
+        {
+            return _groups.AsQueryable();
+        }
+    }
+}
---
+++ GraphQL-.NET/GraphQLDemo/Service/StudentService.cs
@@ -0,0 +1,48 @@
+﻿using GraphQLDemo.Except;
+using GraphQLDemo.IService;
+using GraphQLDemo.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphQLDemo.Service
+{
+    public class StudentService : IStudentService
+    {
+        private IList<Student> _students;
+        public StudentService()
+        {
+            _students = new List<Student>()
+            {
+                new Student() { StudentID = 1, Name = "Vlad", GroupID = 1},
+                new Student() { StudentID = 2, Name = "Andrei", GroupID = 2},
+                new Student() { StudentID = 3, Name = "Sabin", GroupID = 3},
+                new Student() { StudentID = 4, Name = "Marian", GroupID = 1},
+                new Student() { StudentID = 5, Name = "Vlad", GroupID = 3}
+            };
+        }
+        public Student Create(CreateStudentInput inputStudent)
+        {
+            var student = new Student()
+            {
+                StudentID = _students.Max(x => x.StudentID) + 1,
+                Name = inputStudent.Name,
+                GroupID = inputStudent.GroupId
+            };
+            _students.Add(student);
+            return student;
+        }
+
+        public Student Delete(DeleteStudentInput inputStudent)
+        {
+            var student = _students.FirstOrDefault(x => x.StudentID == inputStudent.StudentId);
+            if (student == null) throw new StudentNotFoundException() { StudentId = inputStudent.StudentId };
+            _students.Remove(student);
+            return student;
+        }
+
+        public IQueryable<Student> GetAll()
+        {
+            return _students.AsQueryable();
+        }
+    }
+}
